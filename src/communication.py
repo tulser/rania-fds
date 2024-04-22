17054879e785f658cc8137b77d3f7f3a1ad690ad
wcyates <wcy00001@mix.wvu.edu>
2024-04-22 16:42:08 -0400

    Remove implemented components, keep email extra

--- src/communication.py
+++
@@ -1,7 +0,0 @@
-import configparser
-from comm import FDSSocket
-from domain import FDSDomain
-from fdscommon import FDSDomainConfig, FDSRoomConfig, FDSGlobalConfig, FDSRoomThreadPool
-
-config = configparser.ConfigParser()
-config.read('config.ini')
